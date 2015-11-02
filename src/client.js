7abd93e5ad74daf19f5a05e0c222d9626f4ac473
Krit <krit.h@kaidee.com>
2015-11-02 14:01:15 +0700

    remove something

--- src/client.js
+++ src/client.js
@@ -6,19 +6,10 @@ import {Provider} from 'react-redux';
 import {reduxReactRouter, ReduxRouter} from 'redux-router';
 import AppRoute from './route/AppRoute';
 import createHistory from 'history/lib/createBrowserHistory';
-import { Parse } from 'parse';
 
 const store = ReduxStore('client', window.__data);
 
 const component = <ReduxRouter routes={AppRoute} />;
-
-if(window.location.hostname.indexOf('dev') > -1){
-  // DEV
-  Parse.initialize("D62So7GSAmpKrat7pk72fmJR0BEKFGIgvIR8LQ6t", "7J4ekHlMMSy0wBgh8ERaTzthK6T7WtFgz7hbRBnW");
-}else{
-  // PROD
-  Parse.initialize("YBxAvsMFnbD53ePuj1f2hqfo8mAgzA2Shlmis15t", "uhUMvEF9dAzbvFTbmzuGxrMcnLB1qGqBOWZGLPXs");
-}
 
 render(
   <Provider store={store}>
